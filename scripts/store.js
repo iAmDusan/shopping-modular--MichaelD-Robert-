abc636e5103a72c1aec7103caa313d5e3034e08c
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-06 17:42:43 -0400

    updated store.js with the required functions

--- scripts/store.js
+++ scripts/store.js
@@ -1,7 +1,40 @@
-/* global store, cuid */
+/* global store, cuid, Item */
 'use strict';
 
 const store = (function () {
+  function findById(id) {
+    return store.items.find(item => item.id === id); 
+  }
+  
+  function addItem(name) {
+    try {
+      Item.validateName(name);
+      store.items.push(Item.create(name));
+    } catch(error) {
+      console.log(error.message);
+    }
+
+  }
+  function findAndToggleChecked(id) {
+    
+    !(this.findById(id).checked);
+
+  }
+  function findAndUpdateName(id, newname) {
+    try {
+      Item.validateName(newname);
+      this.findById(id).name = newname;
+
+    } catch(error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+
+  }
+  function findAndDelete(id) {
+
+  }
+
+  
   return {
     items: [
       { id: cuid(), name: 'apples', checked: false },
@@ -10,8 +43,12 @@ const store = (function () {
       { id: cuid(), name: 'bread', checked: false }
     ],
     hideCheckedItems: false,
-    searchTerm: ''
+    searchTerm: '',
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
-
-
 })();
+
